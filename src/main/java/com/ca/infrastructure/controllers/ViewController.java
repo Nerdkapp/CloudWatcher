be8f520529ddeed70b2942c696dbfce7b45256c7
luciano coccia <lcoccia@mac-lcoccia.lan>
2016-03-05 18:01:48 +0100

    Charts mostly working'

--- src/main/java/com/ca/infrastructure/controllers/ViewController.java
+++ src/main/java/com/ca/infrastructure/controllers/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class ViewController {
   @RequestMapping("/")
   String home() {
     return "index";
---
+++ src/main/java/com/ca/infrastructure/resources/MetricsResource.java
@@ -0,0 +1,7 @@
+package com.ca.infrastructure.resources;
+
+/**
+ * Created by lcoccia on 05/03/16.
+ */
+public class MetricsResource {
+}
